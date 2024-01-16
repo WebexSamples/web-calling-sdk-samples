9253d866f7d4e80ad7a005e5d69015a3cce9d5dd
Sreekanth Narayanan <sreenara@cisco.com>
2024-01-16 18:04:48 +0530

    fix(webpack): remove INT urls

--- npm/webpack.config.js
+++ npm/webpack.config.js
@@ -27,26 +27,5 @@ module.exports = {
     new webpack.ProvidePlugin({
       process: "process/browser",
     }),
-    // Environment Plugin doesn't override already defined Environment Variables (i.e. DotENV)
-    new EnvironmentPlugin({
-      WEBEX_LOG_LEVEL: "log",
-      DEBUG: "",
-      // The following environment variables are specific to our continuous
-      // integration process and should not be used in general
-      ATLAS_SERVICE_URL: "https://atlas-intb.ciscospark.com/admin/api/v1",
-      CONVERSATION_SERVICE:
-        "https://conversation-intb.ciscospark.com/conversation/api/v1",
-      ENCRYPTION_SERVICE_URL:
-        "https://encryption-intb.ciscospark.com/encryption/api/v1",
-      HYDRA_SERVICE_URL: "https://apialpha.ciscospark.com/v1/",
-      IDBROKER_BASE_URL: "https://idbrokerbts.webex.com",
-      IDENTITY_BASE_URL: "https://identitybts.webex.com",
-      U2C_SERVICE_URL: "https://u2c-intb.ciscospark.com/u2c/api/v1",
-      WDM_SERVICE_URL: "https://wdm-intb.ciscospark.com/wdm/api/v1",
-      WHISTLER_API_SERVICE_URL:
-        "https://whistler.allnint.ciscospark.com/api/v1",
-      WEBEX_CONVERSATION_DEFAULT_CLUSTER:
-        "urn:TEAM:us-east-1_int13:identityLookup",
-    }),
   ],
 };
